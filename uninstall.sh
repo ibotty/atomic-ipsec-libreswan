552ca826579305a4fa81cdaa6fe82c4550e5b10e
Tobias Florek <tob@butter.sh>
2015-07-08 10:48:42 +0200

    remove superfluous quotes

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-chroot "${HOST}" /usr/bin/systemctl disable /etc/systemd/system/ipsec.service
+chroot ${HOST} /usr/bin/systemctl disable /etc/systemd/system/ipsec.service
 rm -f ${HOST}/etc/systemd/system/ipsec.service
